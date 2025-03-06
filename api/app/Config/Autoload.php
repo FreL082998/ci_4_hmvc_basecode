e82fb7003d432b3662fb9642dd63300ec1a8f196
Freddie Rick Labuanan <frl082998@gmail.com>
2025-03-07 00:20:49 +0800

    chore(be): remove index.php in app config index page and add config path and url helper in condig/autoload

--- api/app/Config/Autoload.php
+++ api/app/Config/Autoload.php
@@ -41,6 +41,7 @@ class Autoload extends AutoloadConfig
      */
     public $psr4 = [
         APP_NAMESPACE => APPPATH,
+        'Config'      => APPPATH . 'Config',
         'App\Commands' => APPPATH . 'Commands',
     ];
 
@@ -91,5 +92,5 @@ class Autoload extends AutoloadConfig
      *
      * @var list<string>
      */
-    public $helpers = [];
+    public $helpers = ['url'];
 }
