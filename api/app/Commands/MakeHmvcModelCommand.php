9a020bc1072b53f38743044075a68279193bb3ff
Freddie Rick Labuanan <frl082998@gmail.com>
2025-03-12 03:58:11 +0800

    chore(be): update the custom command that will generate model and entity class file including generation of migration and seeder file

--- api/app/Commands/MakeHmvcModelCommand.php
+++ api/app/Commands/MakeHmvcModelCommand.php
@@ -52,27 +52,43 @@ class MakeHmvcModelCommand extends BaseCommand
     ];
 
     /**
+     * @var string
+     */
+    protected $modelName;
+    protected $moduleName;
+
+    /**
      * Actually execute a command.
      *
      * @param array $params
      */
     public function run(array $params)
     {
-        $modelName = $params[0] ?? null;
-        $moduleName = CLI::getOption('module');
-
-        if (!$modelName) {
+        $this->modelName = $params[0] ?? null;
+        $this->moduleName = CLI::getOption('module');
+        
+        if (!$this->modelName) {
             CLI::error('You must provide an model name.');
             return;
         }
-
-        if (!$moduleName) {
+        
+        if (!$this->moduleName) {
             CLI::error('You must provide an module name.');
             return;
         }
 
-        $modelFilePath = ROOTPATH . "Modules/{$moduleName}/Models/{$modelName}Model.php";
-        $entityFilePath = ROOTPATH . "Modules/{$moduleName}/Entities/{$modelName}Entity.php";
+        /**
+         * Generate files
+         */
+        $this->createModelFile();
+        $this->createEntityFile();
+        $this->createMigrationFile();
+        $this->createSeederFile();
+    }
+
+    private function createModelFile()
+    {   
+        $modelFilePath = ROOTPATH . "Modules/{$this->moduleName}/Models/{$this->modelName}Model.php";
 
         // Ensure the directory exists before writing the file
         $directory = dirname($modelFilePath);
@@ -81,46 +97,117 @@ class MakeHmvcModelCommand extends BaseCommand
         }
 
         if (file_exists($modelFilePath)) {
-            CLI::error("Model file '{$modelName}Model' already exists.");
+            CLI::error("Model file '{$this->modelName}Model' already exists.");
             return;
         }
-
-        $directory = dirname($entityFilePath);
-        if (!is_dir($directory)) {
-            mkdir($directory, 0755, true);
-        }
-
-        if (file_exists($entityFilePath)) {
-            CLI::error("Entity file '{$modelName}Entity' already exists.");
-            return;
-        }
-
         $modelStubPath = WRITEPATH . 'stubs/hmvc/model_copy.stub';
         if (!file_exists($modelStubPath)) {
             CLI::error('Stub file not found.');
             return;
         }
+        
+        // Load the stub and replace placeholder
+        $modelStubContent = file_get_contents($modelStubPath);
+        $modelContent = str_replace('{{modelName}}', $this->modelName, $modelStubContent);
+        $modelContent = str_replace('{{moduleName}}', $this->moduleName, $modelContent);
 
-        $entityStubPath = WRITEPATH . 'stubs/hmvc/entity.stub';
-        if (!file_exists($entityStubPath)) {
+        file_put_contents($modelFilePath, $modelContent);
+
+        CLI::write("Model file '{$this->modelName}Model' created successfully!", 'green');
+    }
+
+    private function createEntityFile()
+    {
+        $filePath = ROOTPATH . "Modules/{$this->moduleName}/Entities/{$this->modelName}Entity.php";
+
+        // Ensure the directory exists before writing the file
+        $directory = dirname($filePath);
+        if (!is_dir($directory)) {
+            mkdir($directory, 0755, true);
+        }
+
+        if (file_exists($filePath)) {
+            CLI::error("Entity file '{$this->modelName}Entity' already exists.");
+            return;
+        }
+
+        $stubPath = WRITEPATH . 'stubs/hmvc/entity.stub';
+        if (!file_exists($stubPath)) {
             CLI::error('Stub file not found.');
             return;
         }
 
         // Load the stub and replace placeholder
-        $modelStubContent = file_get_contents($modelStubPath);
-        $modelContent = str_replace('{{modelName}}', $modelName, $modelStubContent);
-        $modelContent = str_replace('{{moduleName}}', $moduleName, $modelContent);
+        $stubContent = file_get_contents($stubPath);
+        $content = str_replace('{{moduleName}}', $this->modelName, $stubContent);
 
-        file_put_contents($modelFilePath, $modelContent);
+        file_put_contents($filePath, $content);
 
-        CLI::write("Model file '{$modelName}Model' created successfully!", 'green');
+        CLI::write("Entity file '{$this->modelName}Entity' created successfully!", 'green');
+    }
 
-        $entityStubContent = file_get_contents($entityStubPath);
-        $entityContent = str_replace('{{moduleName}}', $modelName, $entityStubContent);
+    private function createMigrationFile()
+    {
+        $createAt = date('Y-m-d-His');
+        $migrationFileName = "Create{$this->modelName}Table";
+        $filePath = ROOTPATH . "Modules/{$this->moduleName}/Database/Migrations/{$createAt}_{$migrationFileName}.php";
 
-        file_put_contents($entityFilePath, $entityContent);
+        // Ensure the directory exists before writing the file
+        $directory = dirname($filePath);
+        if (!is_dir($directory)) {
+            mkdir($directory, 0755, true);
+        }
 
-        CLI::write("Entity file '{$modelName}Entity' created successfully!", 'green');
+        if (file_exists($filePath)) {
+            CLI::error("Migration file '{$migrationFileName}' already exists.");
+            return;
+        }
+
+        $stubPath = WRITEPATH . 'stubs/hmvc/migration.stub';
+        if (!file_exists($stubPath)) {
+            CLI::error('Stub file not found.');
+            return;
+        }
+
+        // Load the stub and replace placeholder
+        $stubContent = file_get_contents($stubPath);
+        $migrationContent = str_replace('{{migrationFileName}}', $migrationFileName, $stubContent);
+        $migrationContent = str_replace('{{moduleName}}', $this->moduleName, $migrationContent);
+
+        file_put_contents($filePath, $migrationContent);
+
+        CLI::write("Migration file '{$migrationFileName}' created successfully!", 'green');
+    }
+
+    private function createSeederFile()
+    {
+        $seederName = "{$this->modelName}Seeder";
+        $filePath = ROOTPATH . "Modules/{$this->moduleName}/Database/Seeds/{$seederName}.php";
+
+        // Ensure the directory exists before writing the file
+        $directory = dirname($filePath);
+        if (!is_dir($directory)) {
+            mkdir($directory, 0755, true);
+        }
+
+        if (file_exists($filePath)) {
+            CLI::error("Seeder file '{$seederName}' already exists.");
+            return;
+        }
+
+        $stubPath = WRITEPATH . 'stubs/hmvc/seeder.stub';
+        if (!file_exists($stubPath)) {
+            CLI::error('Stub file not found.');
+            return;
+        }
+
+        // Load the stub and replace placeholder
+        $stubContent = file_get_contents($stubPath);
+        $seederContent = str_replace('{{seederName}}', $seederName, $stubContent);
+        $seederContent = str_replace('{{moduleName}}', $this->moduleName, $seederContent);
+
+        file_put_contents($filePath, $seederContent);
+
+        CLI::write("Seeder file '{$seederName}' created successfully!", 'green');
     }
 }
