605c760b7bae25f0b11d481faa659cd4e358eb65
Freddie Rick Labuanan <frl082998@gmail.com>
2025-03-12 02:50:14 +0800

    feat(be): install and init setup ci4 shield

--- api/app/Config/Email.php
+++ api/app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'frl082998@gmail.com';
+    public string $fromName   = 'Freddie Rick Labuanan';
     public string $recipients = '';
 
     /**
